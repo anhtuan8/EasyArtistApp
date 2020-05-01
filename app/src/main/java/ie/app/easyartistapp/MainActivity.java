80fc307824ca9e55bd02e6c2d9dc637f114be764
Anh Tuan <anhtuan.051998@gmail.com>
2020-05-02 01:31:51 +0700

    upgrade Toolbar: using Toolbar widget

--- app/src/main/java/ie/app/easyartistapp/MainActivity.java
+++ app/src/main/java/ie/app/easyartistapp/MainActivity.java
@@ -2,10 +2,12 @@ package ie.app.easyartistapp;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -16,19 +18,44 @@ import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.appbar_menu,menu);
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
 //         Passing each menu ID as a set of Ids because each
 //         menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_camera, R.id.navigation_personal)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-//        Log.v("Action bar", String.valueOf(getSupportActionBar().getTitle()));
+//        NavigationUI.setupActionBarWithNavController(toolbar, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView,navController);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_dehaze_black_24dp);
+        Log.v("Toolbar", String.valueOf(toolbar.getMenu().size()));
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.appbarbutton_search:
+                //search event
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
