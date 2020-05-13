90e66e5b78ef310d5d7b1f7a875c4642fa00c712
Anh Tuan <anhtuan.051998@gmail.com>
2020-05-13 11:41:01 +0700

    In Personal: add TabLayout with demo child fragment (DemoPersonalFragment)

--- app/src/main/java/ie/app/easyartistapp/ui/personal/PersonalFragment.java
+++ app/src/main/java/ie/app/easyartistapp/ui/personal/PersonalFragment.java
@@ -1,6 +1,7 @@
 package ie.app.easyartistapp.ui.personal;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import ie.app.easyartistapp.R;
 
 public class PersonalFragment extends Fragment {
+    private static final String TAG = "PersonalFragment";
 
     private PersonalViewModel personalViewModel;
+    private DemoPersonalFragment demoPersonalFragment;
+    private PersonalAdapter personalAdapter;
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,7 +40,21 @@ public class PersonalFragment extends Fragment {
 //                textView.setText(s);
 //            }
 //        });
-
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        personalAdapter = new PersonalAdapter(this);
+        viewPager = view.findViewById(R.id.pager);
+        viewPager.setAdapter(personalAdapter);
+
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText("OBJECT" + (position + 1));
+            }
+        }).attach();
     }
 }
---
+++ app/src/main/java/ie/app/easyartistapp/ui/personal/favourite/PersonalFavouriteFragment.java
@@ -0,0 +1,25 @@
+package ie.app.easyartistapp.ui.personal.favourite;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ie.app.easyartistapp.R;
+
+public class PersonalFavouriteFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_personal_favourite,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
