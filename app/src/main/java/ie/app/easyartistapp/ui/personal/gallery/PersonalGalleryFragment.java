5359efc52824704f03a42d114003cb9823ddad1b
LeCongThuong <thuongle276@gmail.com>
2020-07-01 23:20:25 +0700

    show image in gallery

--- app/src/main/java/ie/app/easyartistapp/ui/personal/gallery/PersonalGalleryFragment.java
+++ app/src/main/java/ie/app/easyartistapp/ui/personal/gallery/PersonalGalleryFragment.java
@@ -1,6 +1,8 @@
 package ie.app.easyartistapp.ui.personal.gallery;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,44 +14,37 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import ie.app.easyartistapp.R;
 
 public class PersonalGalleryFragment extends Fragment {
     private GalleryViewModel galleryViewModel = new GalleryViewModel();
-    private ArrayList<String> imagePaths = new ArrayList<>();
+    private File[] imagePaths = null;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_personal_gallery,container,false);
         //imagePaths = galleryViewModel.getImagePaths();
+
         createPathList();
         initRecyclerView(root);
         return root;
     }
 
     private void createPathList(){
-        imagePaths.add("ie/app/easyartistapp/images/1.png");
-        imagePaths.add("ie/app/easyartistapp/images/2.png");
-        imagePaths.add("ie/app/easyartistapp/images/3.png");
-        imagePaths.add("ie/app/easyartistapp/images/4.png");
-        imagePaths.add("ie/app/easyartistapp/images/5.png");
-        imagePaths.add("ie/app/easyartistapp/images/6.png");
-        imagePaths.add("ie/app/easyartistapp/images/7.png");
-        imagePaths.add("ie/app/easyartistapp/images/8.png");
-        imagePaths.add("ie/app/easyartistapp/images/9.png");
-        imagePaths.add("ie/app/easyartistapp/images/10.png");
-        imagePaths.add("ie/app/easyartistapp/images/11.png");
-        imagePaths.add("ie/app/easyartistapp/images/12.png");
-        imagePaths.add("ie/app/easyartistapp/images/13.png");
+
+        File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picture/");
+        imagePaths= dir.listFiles();
+
     }
 
     public void initRecyclerView(View view){
         RecyclerView recyclerView = view.findViewById(R.id.personal_gallery);
         GalleryRecyclerViewAdapter adapter = new GalleryRecyclerViewAdapter(getContext(),imagePaths);
         recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 5));
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
     }
 }
