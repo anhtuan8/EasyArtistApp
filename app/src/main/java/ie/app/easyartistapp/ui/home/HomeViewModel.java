510027b954a29b726e60c9efd68e52626efeb529
Anh Tuan <anhtuan.051998@gmail.com>
2020-07-01 10:47:46 +0700

    fix: can not fetch from firebase

--- app/src/main/java/ie/app/easyartistapp/ui/home/HomeViewModel.java
+++ app/src/main/java/ie/app/easyartistapp/ui/home/HomeViewModel.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
