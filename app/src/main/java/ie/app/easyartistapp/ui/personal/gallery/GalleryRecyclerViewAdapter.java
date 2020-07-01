b174428bddfd3461a3a98b652812c3af30dd1207
LeCongThuong <thuongle276@gmail.com>
2020-07-02 00:26:44 +0700

    can view stylized image with default viewer

--- app/src/main/java/ie/app/easyartistapp/ui/personal/gallery/GalleryRecyclerViewAdapter.java
+++ app/src/main/java/ie/app/easyartistapp/ui/personal/gallery/GalleryRecyclerViewAdapter.java
@@ -1,8 +1,11 @@
 package ie.app.easyartistapp.ui.personal.gallery;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -18,6 +22,8 @@ import java.io.File;
 import java.util.ArrayList;
 
 import ie.app.easyartistapp.R;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 public class GalleryRecyclerViewAdapter extends RecyclerView.Adapter<GalleryViewHolder> {
     private static final String TAG = "GalleryRecyclerViewAdap";
@@ -41,14 +47,23 @@ public class GalleryRecyclerViewAdapter extends RecyclerView.Adapter<GalleryView
 
     @Override
     public void onBindViewHolder(@NonNull GalleryViewHolder holder, final int position) {
+        File imagePath = imagePaths[position];
         holder.getGalleryItem().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: Clicked on pictures number " + position);
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setDataAndType(Uri.fromFile(imagePath), "image/*");
+//                context.startActivity(intent);
+                final Intent intent = new Intent(Intent.ACTION_VIEW)//
+                        .setDataAndType(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ?
+                                        FileProvider.getUriForFile(context, getApplicationContext().getPackageName()+ ".provider", imagePath) : Uri.fromFile(imagePath),
+                                "image/*").addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                context.startActivity(intent);
             }
         });
         //setBitmapImagesFromFile(holder.getGalleryItemImage(),imagePaths.get(position));
-        File imagePath = imagePaths[position];
+
         Glide.with(context)
                 .load(imagePath) // Uri of the picture
                 .override(200, 200)
